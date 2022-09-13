5ba514c31d2f401005f1550f3aac5cace6e0c617
bullmoosedev <koleervine@gmail.com>
2022-09-12 21:32:54 -0600

    created react app

---
+++ us-arboretum/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+
+          <br></br><br></br>
+          You will want to update the css to make the landing page look good, but this is a modest start for making the arboretum.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
