d7f9ecc3a48aa4af02efad70f7107767a56194a3
bullmoosedev <koleervine@gmail.com>
2022-09-14 13:10:21 -0600

    added some react lines for displaying the trees

--- us-arboretum/src/App.js
+++ us-arboretum/src/App.js
@@ -1,26 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import AmericanCherry from './assets/AmericanCherry.png';
+
+function importAllImages(i) {
+  let images = {};
+  i.keys().map((item, index) => { images[item.replace('./', '')] = i(item); });
+  return images;
+}
+
+const images = importAllImages(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-          <br></br><br></br>
-          You will want to update the css to make the landing page look good, but this is a modest start for making the arboretum.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={images['AmericanCherry.png']} />
     </div>
   );
 }
